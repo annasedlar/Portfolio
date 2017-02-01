c5a713c32c15ed454aff57e4cdc2509d09bcafc9
Anna Sedlar <annasedlar@gmail.com>
2017-02-01 11:52:35 -0500

    changing navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,10 +7,9 @@ import li from './sm-icons/linkedin.png';
 import git from './sm-icons/github.png';
 import mail from './sm-icons/mail.png';
 import tw from './sm-icons/twitter.png';
+import {Modal, NavBar, ButtonDropdown} from 'simple-react-bootstrap'
 
-
-//FIX COLLAPSE BUTTON
-
+// FIX COLLAPSE BUTTON
 class Navbar extends Component{
     render(){
         return(
@@ -45,8 +44,50 @@ class Navbar extends Component{
     }
 }
 
-
-
+{/*class Navbar extends Component{
+    render(){
+        return(
+            <NavBar>
+                <NavBar.Header>
+                    <NavBar.Brand>
+                        <a style={{ cursor: 'pointer' }}>Anna Sedlar</a>
+                    </NavBar.Brand>
+                    <NavBar.Toggle />
+                </NavBar.Header>
+                <NavBar.Nav>
+                    <NavBar.Item className="class-on-item">Link 1</NavBar.Item>
+                    <NavBar.Item disabled={true}>Link 2</NavBar.Item>
+                    <NavBar.Item active={true} href="http://www.google.com">Link 3</NavBar.Item>
+                    <NavBar.Dropdown toggleClassName="pointer-cursor" style={{ color: 'red' }} text="Sub menu">
+                        <NavBar.Item>Sub option a</NavBar.Item>
+                        <NavBar.Item href="#foo">Sub option b</NavBar.Item>
+                        <NavBar.ItemDivider />
+                        <NavBar.Item>Sub option c</NavBar.Item>
+                    </NavBar.Dropdown>
+                    <NavBar.Dropdown disabled={true} text="Sub menu">
+                        <NavBar.Item>Sub option a</NavBar.Item>
+                        <NavBar.Item>Sub option b</NavBar.Item>
+                        <NavBar.Item>Sub option c</NavBar.Item>
+                    </NavBar.Dropdown>
+                </NavBar.Nav>
+                <NavBar.Header>
+                    <NavBar.Brand>
+                        <a style={{ cursor: 'pointer' }}>Second Header</a>
+                    </NavBar.Brand>
+                    <NavBar.Toggle />
+                </NavBar.Header>
+                <NavBar.Form className="pull-left">
+                    <div className="form-group">
+                        <div className="input-group">
+                                <span className="input-group-btn">
+                                    <button className="btn default">Search</button>
+                                </span>
+                            <input className="form-control" placeholder="Quick title search" />
+                        </div>
+                    </div>
+                </NavBar.Form>
+            </NavBar>
+        )}}*/}
 
 
 export default Navbar;
